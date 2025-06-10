e7d068674b76e2142b838c95019986ab9d7fb08f
Viserate <135495527+Viserate@users.noreply.github.com>
2025-06-09 22:42:23 -0500

    Update installer.lua

--- ReactorPlus/installer.lua
+++ ReactorPlus/installer.lua
@@ -1,36 +1,31 @@
--- Installer for ReactorPlus
+-- Safe installer for low-space CC systems
 local files = {
-    "config.lua",
-    "main.lua",
-    "pid.lua",
-    "input.lua",
-    "ui.lua",
-    "helpers.lua",
-    "peripheral_scan.lua",
-    "state.lua"
+  "config.lua", "main.lua", "pid.lua",
+  "input.lua", "ui.lua", "helpers.lua",
+  "peripheral_scan.lua", "state.lua"
 }
 
-local baseUrl = "https://github.com/Viserate/ReactorPlus/tree/main/ReactorPlus/"
-
+local base = "https://raw.githubusercontent.com/Viserate/ReactorPlus/main/ReactorPlus/"
 fs.makeDir("ReactorPlus")
 
 for _, file in ipairs(files) do
-    local res = http.get(baseUrl .. file)
-    if res then
-        local handle = fs.open("ReactorPlus/" .. file, "w")
-        handle.write(res.readAll())
-        handle.close()
-        print("Installed: " .. file)
-    else
-        print("Failed to download: " .. file)
-    end
+  local url = base .. file
+  local res = http.get(url)
+  if res then
+    local handle = fs.open("ReactorPlus/" .. file, "w")
+    handle.write(res.readAll())
+    handle.close()
+    print("Installed:", file)
+  else
+    print("Failed to download:", file)
+  end
 end
 
--- Create startup.lua
+-- Create startup file
 local startup = fs.open("startup.lua", "w")
-startup.write('shell.run("ReactorPlus/main.lua")\n')
+startup.write('shell.run("ReactorPlus/main.lua")')
 startup.close()
 
-print("ReactorPlus installed! Rebooting...")
+print("Install complete. Rebooting...")
 sleep(2)
 os.reboot()
