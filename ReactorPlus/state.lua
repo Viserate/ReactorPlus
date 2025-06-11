3517ccb92e58ccb39d6cbdecce7ea23b45a6da0b
Viserate <135495527+Viserate@users.noreply.github.com>
2025-06-10 22:35:09 -0500

    Update state.lua

--- ReactorPlus/state.lua
+++ ReactorPlus/state.lua
@@ -1,10 +1,24 @@
+-- /ReactorPlus/state.lua
 local t = 0
+local statusToggle = true
+
 return {
     getEnergyPercent = function()
         t = (t + 0.01) % 1
         return t
     end,
+
     getTemperature = function()
         return math.floor(300 + math.sin(t * math.pi * 2) * 100)
+    end,
+
+    getReactorStatus = function()
+        return statusToggle and "ONLINE" or "OFFLINE"
+    end,
+
+    toggleStatus = function()
+        statusToggle = not statusToggle
     end
 }
+
+
